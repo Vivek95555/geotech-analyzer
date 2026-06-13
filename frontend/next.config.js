eccb79592c01325feced2c7b3497e345d8ed2af5
Vivek Solanki <vivek.solanki@exovanceglobal.com>
2026-06-13 14:22:04 +0530

    fix api url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:5000/:path*", // Proxy to Flask backend
-      },
-    ];
-  },
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
